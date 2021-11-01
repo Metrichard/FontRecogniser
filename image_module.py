5dbeaf555c1c9d1b4685fc3d9118edb14f353767
Baráth <richardqwertzu@gmail.com>
2021-11-01 15:25:52 +0100

    feat: added pixel colour inverter

--- image_module.py
+++ image_module.py
@@ -3,11 +3,23 @@ import os.path
 
 
 class image_module:
-    def __init__(self):
-        pass
-
-    def load_image(self, filepath):
+    def __init__(self, filepath):
+        self.stored_image = None
         if os.path.isfile(filepath):
-            return Image.open(filepath)
+            self.stored_image = Image.open(filepath)
         else:
             print("The file either does not exist or the path is wrong")
+
+    def get_stored_image(self):
+        return self.stored_image
+
+    def invert_colours_of_image(self):
+        if self.stored_image:
+            width, height = self.stored_image.size
+            for x in range(width):
+                for y in range(height):
+                    r, g, b, _ = self.stored_image.getpixel((x, y))
+                    if r < 30 and g < 30 and b < 30:
+                        self.stored_image.putpixel((x, y), (255, 255, 255))
+                    else:
+                        self.stored_image.putpixel((x, y), (0, 0, 0))
--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import image_module
 
-file_path = r"../FontRecogniser/TestPictures/test_picture.png";
+file_path = r"../FontRecogniser/TestPictures/test_picture.jpg"
 
-imageLoader = image_module.image_module()
+imageLoader = image_module.image_module(file_path)
+imageLoader.invert_colours_of_image()
 
-image = imageLoader.load_image(file_path)
-
-image.show()
+imageLoader.stored_image.show()
